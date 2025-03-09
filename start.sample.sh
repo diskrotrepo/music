aa9450e14924e3ee38aff672c443d1ce931482f6
Alex Ayers <alex.ayers@diskrot.com>
2025-03-09 16:52:54 -0400

    comment on sample start script

--- start.sample.sh
+++ start.sample.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PYTHONPATH:$PWD
 export CUDA_VISIBLE_DEVICES=0
-export OPENAI_API_KEY=lm-studio
-export USE_LOCAL_LLM=true
-export LLM_API=http://192.168.7.216:1234/v1
-export MODEL=llama-3.2-3b-instruct
+export OPENAI_API_KEY=lm-studio #Or your OpenAI API Key
+export USE_LOCAL_LLM=true # false if using OpenAI
+export LLM_API=http://192.168.7.216:1234/v1 # ignored when using OpenAI
+export MODEL=llama-3.2-3b-instruct # The model you want to use important for local or OpenAI
 
 conda activate diffrhythm
 pip install -r requirements.txt
