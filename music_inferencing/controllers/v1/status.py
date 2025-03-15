2c053703581399278f5364214b497f60b71966a0
Alex Ayers <me@alexayers.com>
2025-03-15 10:57:26 -0400

    copyright notice

--- music_inferencing/controllers/v1/status.py
+++ music_inferencing/controllers/v1/status.py
@@ -1,7 +1,14 @@
-from enum import Enum
+# Copyright (c) 2025 diskrot
+#               2025 Alex Ayers   (alex.ayers@diskrot.com)
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from flask_restx import Namespace, Resource, fields
 from flask import request
-from music_inferencing.background_thread import BackgroundThreadFactory
 from music_inferencing.extensions import db
 from music_inferencing.models import Music
 
